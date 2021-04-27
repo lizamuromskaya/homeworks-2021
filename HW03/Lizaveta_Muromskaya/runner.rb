1c8aed7ab4cd2dfb1bf5c5e7c87911ec7b25b10e
lizamuromskaya <lizavetamuromskaya@gmail.com>
2021-04-27 22:33:05 +0300

    [HW03] Launch

---
+++ HW03/Lizaveta_Muromskaya/runner.rb
@@ -0,0 +1,31 @@
+require_relative 'lizaveta_muromskaya_hw_03_t_01'
+require_relative 'lizaveta_muromskaya_hw_03_t_02'
+require_relative 'lizaveta_muromskaya_hw_03_t_03'
+require_relative 'lizaveta_muromskaya_hw_03_t_04'
+
+log1 = '10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0498
+10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] "POST /grid/2/event HTTP/1.1" 200 - 0.2277
+2018-04-23 20:30:42: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System error: Undefined error: 0 - 0>
+10.6.246.101 - - [23/Apr/2018:20:29:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0290'
+
+log2 = '10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /test/2/messages HTTP/1.1" 200 48 0.0498
+10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] "POST /test/2/run HTTP/1.1" 200 - 0.2277
+2018-04-23 20:30:42: SSL ERROR, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSL: System error: Undefined error: 0 - 0>
+10.6.246.101 - - [23/Apr/2018:20:31:39 +0300] "POST /test/2/messages HTTP/1.1" 200 48 0.0290'
+
+log3 = '2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - Calling core with action: event
+2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
+2018-04-23 17:17:49.8 ubuntu-xenial[14319] Debug - docker event processed
+2018-04-23 17:18:19.5 ubuntu-xenial[14319] Debug - monitoring grid communication health
+2018-04-23 17:18:38.8 ubuntu-xenial[14319] Debug - Calling core with action: messages
+2018-04-23 17:18:38.8 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
+2018-04-23 17:18:59.8 ubuntu-xenial[14319] Debug - inside docker_handle_event'
+
+puts '-----TASK 1-----'
+task_1(log1)
+puts '-----TASK 2-----'
+task_2(log2)
+puts '-----TASK 3-----'
+task_3(log3)
+puts '-----TASK 4-----'
+task_4('sA246a')
