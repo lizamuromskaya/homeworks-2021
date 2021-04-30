339c1fff46c04dbc1efe5e17b80761aeba8a1c2d
lizamuromskaya <lizavetamuromskaya@gmail.com>
2021-04-30 12:43:42 +0300

    [HW03] Update Task 4. Character counter

--- HW03/Lizaveta_Muromskaya/lizaveta_muromskaya_hw_03_t_04.rb
+++ HW03/Lizaveta_Muromskaya/lizaveta_muromskaya_hw_03_t_04.rb
@@ -4,5 +4,5 @@ def task4(data)
   character_counter = {}
   character_counter[:letters] = data.count('A-Za-z')
   character_counter[:digits] = data.count('0-9')
-  p character_counter
+  character_counter
 end
