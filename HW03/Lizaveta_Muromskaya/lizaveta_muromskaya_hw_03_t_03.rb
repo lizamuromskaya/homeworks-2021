0a74a1015bcb76758469bd5e5bb496c20154b2d3
lizamuromskaya <lizavetamuromskaya@gmail.com>
2021-04-30 19:31:15 +0300

    [HW-03] 3 task wrapped in a class

--- HW03/Lizaveta_Muromskaya/lizaveta_muromskaya_hw_03_t_03.rb
+++ HW03/Lizaveta_Muromskaya/lizaveta_muromskaya_hw_03_t_03.rb
@@ -1,30 +1,37 @@
 # frozen_string_literal: true
 
-TIME = /[0-9]+:[0-9]+:[0-9]+\.[0-9]/.freeze
-def time_in_sec(time, seconds)
-  time.each do |time_numbers|
-    time_buf = time_numbers.split(':')
-    sec = time_buf[0].to_f * 3600 + time_buf[1].to_f * 60 + time_buf[2].to_f
-    seconds << sec
+# This is class for task3
+class Task
+  TIME = /[0-9]+:[0-9]+:[0-9]+\.[0-9]/.freeze
+  def time_in_sec(time)
+    time.each do |time_numbers|
+      time_buf = time_numbers.split(':')
+      sec = time_buf[0].to_f * 3600 + time_buf[1].to_f * 60 + time_buf[2].to_f
+      @seconds << sec
+    end
+    @seconds
   end
-end
 
-def time_difference(seconds)
-  seconds.each_cons(2).map do |start_event, end_event|
-    res = end_event - start_event
-    res.positive? ? res.round(1).to_s : nil
-  end.compact
-end
+  def time_difference
+    @seconds.each_cons(2) do |start_event, end_event|
+      res = end_event - start_event
+      # @result << res.round(1).to_s if res.positive?
+      @result << res.round(1).to_s if res.positive?
+    end
+    @result
+  end
 
-def task3(log)
-  seconds = []
+  def task3(log)
+    @seconds = []
+    @result = []
+    lines = log.split("\n")
+    lines.each do |string|
+      time = string.scan(TIME) if string.include?('Calling core with action:')
+      next if time.nil?
 
-  lines = log.split("\n")
-  lines.each do |string|
-    time = string.scan(TIME) if string.include?('Calling core with action:')
-    next if time.nil?
-
-    time_in_sec(time, seconds)
-    time_difference(seconds)
+      time_in_sec(time)
+      time_difference
+    end
+    @result
   end
 end
