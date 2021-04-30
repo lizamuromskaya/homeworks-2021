fa6494bd73148f8f9abbef69b8940c9ee812c5b8
lizamuromskaya <lizavetamuromskaya@gmail.com>
2021-04-30 14:25:41 +0300

    [HW03] Update Task 3. Calculating seconds between events

--- HW03/Lizaveta_Muromskaya/lizaveta_muromskaya_hw_03_t_03.rb
+++ HW03/Lizaveta_Muromskaya/lizaveta_muromskaya_hw_03_t_03.rb
@@ -1,31 +1,30 @@
 # frozen_string_literal: true
 
 TIME = /[0-9]+:[0-9]+:[0-9]+\.[0-9]/.freeze
-def time_in_sec(time)
+def time_in_sec(time, seconds)
   time.each do |time_numbers|
     time_buf = time_numbers.split(':')
     sec = time_buf[0].to_f * 3600 + time_buf[1].to_f * 60 + time_buf[2].to_f
-    @seconds << sec
+    seconds << sec
   end
 end
 
-def time_difference
-  @seconds.each_cons(2) do |start_event, end_event|
+def time_difference(seconds)
+  seconds.each_cons(2).map do |start_event, end_event|
     res = end_event - start_event
-    @result << res.round(1).to_s if res.positive?
-  end
+    res.positive? ? res.round(1).to_s : nil
+  end.compact
 end
 
 def task3(log)
-  @seconds = []
-  @result = []
+  seconds = []
+
   lines = log.split("\n")
   lines.each do |string|
     time = string.scan(TIME) if string.include?('Calling core with action:')
     next if time.nil?
 
-    time_in_sec(time)
-    time_difference
+    time_in_sec(time, seconds)
+    time_difference(seconds)
   end
-  @result
 end
