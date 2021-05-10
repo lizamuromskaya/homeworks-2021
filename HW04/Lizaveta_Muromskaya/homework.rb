86c8ed7957f99862fd14ba5e05142906a30828ba
lizamuromskaya <lizavetamuromskaya@gmail.com>
2021-05-10 16:52:52 +0300

    [HW-04] Refactoring class Human

--- HW04/Lizaveta_Muromskaya/homework.rb
+++ HW04/Lizaveta_Muromskaya/homework.rb
@@ -3,9 +3,12 @@
 # class homework
 class Homework
   attr_reader :title, :data
+  attr_accessor :answer
 
-  def initialize(title:, data:)
+  def initialize(title:, data:, student:, answer: nil)
     @title = title
     @data = data
+    @student = student
+    @answer = answer
   end
 end
