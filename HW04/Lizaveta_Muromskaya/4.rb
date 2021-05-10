6ec697af14cf378a5640e9ef38192e8d714f4a89
lizamuromskaya <lizavetamuromskaya@gmail.com>
2021-05-10 19:14:26 +0300

    [HW-04] Interaction between students and mentors

--- HW04/Lizaveta_Muromskaya/4.rb
+++ HW04/Lizaveta_Muromskaya/4.rb
@@ -6,38 +6,38 @@ require './student'
 student = Student.new(name: 'John', surname: 'Doe')
 mentor = Mentor.new(name: 'Jack', surname: 'Gonsales')
 
-mentor.task_for_student('HW:05', 'data', student)
+homework = mentor.task_for_student('HW:05', 'data', student)
 
-student.add_answer('Data1')
+student.add_answer(homework, 'Data1')
 student.to_check
 
 mentor.subscribe_to(student)
 
-mentor.add_new_task
+mentor.add_new_task(homework)
 student.show_notifications
 student.read_notifications
 
 mentor.show_notifications # => []
 
-student.add_answer('Data2')
+student.add_answer(homework, 'Data2')
 student.to_check
 mentor.show_notifications # => [Notification, ...]
 
 mentor.read_notifications
 mentor.show_notifications # => []
 
-student.add_answer('Data3')
+student.add_answer(homework, 'Data3')
 student.to_check
 
-mentor.reject_to_work
+mentor.reject_to_work(homework)
 
 student.show_notifications
 student.read_notifications
 
-student.add_answer('Data4')
+student.add_answer(homework, 'Data4')
 student.to_check
 
-mentor.accept_to_work
+mentor.accept_to_work(homework)
 
 student.show_notifications
 student.read_notifications
