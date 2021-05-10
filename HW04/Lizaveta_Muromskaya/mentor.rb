c8a8e6b6160cdf8e48b743a54dd4292ac637a0de
lizamuromskaya <lizavetamuromskaya@gmail.com>
2021-05-10 16:56:14 +0300

    [HW-04] Refactoring class Mentor

--- HW04/Lizaveta_Muromskaya/mentor.rb
+++ HW04/Lizaveta_Muromskaya/mentor.rb
@@ -3,8 +3,11 @@
 require './human'
 # The class that is responsible for the mentor's actions
 class Mentor < Human
-  attr_reader :notifications
-  attr_accessor :students
+  private
+
+  attr_reader :students
+
+  public
 
   def initialize(name:, surname:)
     super
@@ -15,6 +18,18 @@ class Mentor < Human
   def subscribe_to(student)
     student.mentors << self
     @students << student
+  end
+
+  def task_for_student(title, data, student)
+    homework = Homework.new(title: title, data: data, student: student)
+    student.to_work(homework)
+  end
+
+  def add_new_task
+    notification = Notification.new(message: 'Add a new task!')
+    @students.each do |student|
+      student.notifications << notification
+    end
   end
 
   def reject_to_work
