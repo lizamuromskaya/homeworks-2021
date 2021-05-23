07c7eed7e740baa1662240c8ba69f50ed7ea6d54
lizamuromskaya <lizavetamuromskaya@gmail.com>
2021-05-23 21:55:59 +0300

    Refactoring specs for task 2

--- HW03/Lizaveta_Muromskaya/spec/lizaveta_muromskaya_hw_03_t_02_spec.rb
+++ HW03/Lizaveta_Muromskaya/spec/lizaveta_muromskaya_hw_03_t_02_spec.rb
@@ -1,51 +1,28 @@
 # frozen_string_literal: true
 
 require 'rspec'
-require_relative './lizaveta_muromskaya_hw_03_t_02'
-
-log = <<~LOGS
-  10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /test/2/messages HTTP/1.1" 200 48 0.0498
-  10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] "POST /test/2/run HTTP/1.1" 200 - 0.2277
-  2018-04-23 20:30:42: SSL ERROR, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSL: System error: Undefined error: 0 - 0>
-  10.6.246.101 - - [23/Apr/2018:20:31:39 +0300] "POST /test/2/messages HTTP/1.1" 200 48 0.0290
-LOGS
-log_in_the_wrong_format = <<~LOGS
-   10.6.246.103 - - "POST /test/2/messages HTTP/1.1" 200 48 0.0498
-  10.6.246.101 - -"POST /test/2/run HTTP/1.1" 200 - 0.2277
-  2018-04-23 20:30:42: SSL ERROR, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSL: System error: Undefined error: 0 - 0>
-  10.6.246.101 - -"POST /test/2/messages HTTP/1.1" 200 48 0.0290
-LOGS
-log_with_errors = <<~LOGS
-  10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /test/2/messages HTTP/1.1" 200 48 0.0498
-  2018-04-23 20:30:42: SSL ERROR, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSL: System error: Undefined error: 0 - 0>
-  10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] "POST /test/2/run HTTP/1.1" 200 - 0.2277
-  2018-04-23 20:30:42: SSL ERROR, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSL: System error: Undefined error: 0 - 0>
-  10.6.246.101 - - [23/Apr/2018:20:31:39 +0300] "POST /test/2/messages HTTP/1.1" 200 48 0.0290
-  2020-04-23 SSL ERROR, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSL: System error: Undefined error: 0 - 0>
-LOGS
-log_without_errors = <<~LOGS
-  10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /test/2/messages HTTP/1.1" 200 48 0.0498
-  10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] "POST /test/2/run HTTP/1.1" 200 - 0.2277
-  10.6.246.101 - - [23/Apr/2018:20:31:39 +0300] "POST /test/2/messages HTTP/1.1" 200 48 0.0290
-LOGS
-
-right_output = [
-  'DATE: 23/Apr/2018:20:30:39 +0300 FROM: 10.6.246.103 TO: /TEST/2/MESSAGES',
-  'DATE: 23/Apr/2018:20:30:42 +0300 FROM: 10.6.246.101 TO: /TEST/2/RUN',
-  'DATE: 23/Apr/2018:20:31:39 +0300 FROM: 10.6.246.101 TO: /TEST/2/MESSAGES'
-]
+require_relative '../lizaveta_muromskaya_hw_03_t_02'
 
 RSpec.describe 'Array of formatted strings containing information about post requests' do
   describe 'Log that has wrong output' do
     context 'when text are not given' do
       it 'return an empty array' do
-        expect(task2('')).to  eq([])
+        expect(task2('')).to eq([])
       end
     end
 
     context 'when text given in wrong format' do
+      let(:log) do
+        <<~LOGS
+           10.6.246.103 - - "POST /test/2/messages HTTP/1.1" 200 48 0.0498
+          10.6.246.101 - -"POST /test/2/run HTTP/1.1" 200 - 0.2277
+          2018-04-23 20:30:42: SSL ERROR, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSL: System error: Undefined error: 0 - 0>
+          10.6.246.101 - -"POST /test/2/messages HTTP/1.1" 200 48 0.0290
+        LOGS
+      end
+
       it 'return an empty array' do
-        expect(task2(log_in_the_wrong_format)).to eq([])
+        expect(task2(log)).to eq([])
       end
     end
 
@@ -57,21 +34,72 @@ RSpec.describe 'Array of formatted strings containing information about post req
   end
 
   describe 'Log that has right output' do
+    subject { task2(log) }
+
     context 'when text given in right format' do
+      let(:log) do
+        <<~LOGS
+          10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /test/2/messages HTTP/1.1" 200 48 0.0498
+          10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] "POST /test/2/run HTTP/1.1" 200 - 0.2277
+          2018-04-23 20:30:42: SSL ERROR, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSL: System error: Undefined error: 0 - 0>
+          10.6.246.101 - - [23/Apr/2018:20:31:39 +0300] "POST /test/2/messages HTTP/1.1" 200 48 0.0290
+        LOGS
+      end
+      let(:right_output) do
+        [
+          'DATE: 23/Apr/2018:20:30:39 +0300 FROM: 10.6.246.103 TO: /TEST/2/MESSAGES',
+          'DATE: 23/Apr/2018:20:30:42 +0300 FROM: 10.6.246.101 TO: /TEST/2/RUN',
+          'DATE: 23/Apr/2018:20:31:39 +0300 FROM: 10.6.246.101 TO: /TEST/2/MESSAGES'
+        ]
+      end
+
       it 'return an array of formatted strings' do
-        expect(task2(log)).to eq(right_output)
+        expect(subject).to eq(right_output)
       end
     end
 
     context 'when logs have more than one error' do
+      let(:log) do
+        <<~LOGS
+          10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /test/2/messages HTTP/1.1" 200 48 0.0498
+          2018-04-23 20:30:42: SSL ERROR, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSL: System error: Undefined error: 0 - 0>
+          10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] "POST /test/2/run HTTP/1.1" 200 - 0.2277
+          2018-04-23 20:30:42: SSL ERROR, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSL: System error: Undefined error: 0 - 0>
+          10.6.246.101 - - [23/Apr/2018:20:31:39 +0300] "POST /test/2/messages HTTP/1.1" 200 48 0.0290
+          2020-04-23 SSL ERROR, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSL: System error: Undefined error: 0 - 0>
+        LOGS
+      end
+      let(:right_output) do
+        [
+          'DATE: 23/Apr/2018:20:30:39 +0300 FROM: 10.6.246.103 TO: /TEST/2/MESSAGES',
+          'DATE: 23/Apr/2018:20:30:42 +0300 FROM: 10.6.246.101 TO: /TEST/2/RUN',
+          'DATE: 23/Apr/2018:20:31:39 +0300 FROM: 10.6.246.101 TO: /TEST/2/MESSAGES'
+        ]
+      end
+
       it 'return an array of formatted strings' do
-        expect(task2(log_with_errors)).to eq(right_output)
+        expect(subject).to eq(right_output)
       end
     end
 
     context 'when logs have no lines with error' do
+      let(:log) do
+        <<~LOGS
+          10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /test/2/messages HTTP/1.1" 200 48 0.0498
+          10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] "POST /test/2/run HTTP/1.1" 200 - 0.2277
+          10.6.246.101 - - [23/Apr/2018:20:31:39 +0300] "POST /test/2/messages HTTP/1.1" 200 48 0.0290
+        LOGS
+      end
+      let(:right_output) do
+        [
+          'DATE: 23/Apr/2018:20:30:39 +0300 FROM: 10.6.246.103 TO: /TEST/2/MESSAGES',
+          'DATE: 23/Apr/2018:20:30:42 +0300 FROM: 10.6.246.101 TO: /TEST/2/RUN',
+          'DATE: 23/Apr/2018:20:31:39 +0300 FROM: 10.6.246.101 TO: /TEST/2/MESSAGES'
+        ]
+      end
+
       it 'return an array of formatted strings' do
-        expect(task2(log_without_errors)).to eq(right_output)
+        expect(subject).to eq(right_output)
       end
     end
   end
